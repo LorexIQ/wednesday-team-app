45dd8d5929ae38f51814d0e0ba84d8b8fc73110b
LorexIQ <murashko100500@gmail.com>
2023-04-13 22:45:44 +0300

    feat: add auto version complete

--- client/application/app/src/main/java/it/bgitu/wednesday/network/Const.kt
+++ client/application/app/src/main/java/it/bgitu/wednesday/network/Const.kt
@@ -3,7 +3,7 @@ package it.bgitu.wednesday.network
 import it.bgitu.wednesday.network.users.UsersResponseBodyDto
 
 object Const {
-    const val BASE_URL = "http://192.168.6.135:4000/api/"
+    const val BASE_URL = "http://server.lorexiq.ru:4000/api/"
     const val TOKEN_CACHE: String = "TOKEN"
     var TOKEN: String = ""
     var ME: UsersResponseBodyDto? = null
